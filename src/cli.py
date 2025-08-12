324360f3afa6c1b1b456c1257a9098fd7e689b33
Chrisando Ryan <chrisando.pardomuan@skyscanner.net>
2025-08-12 11:23:16 +0100

    Bump to version 1.8.0

--- src/cli.py
+++ src/cli.py
@@ -3,7 +3,7 @@
 
 __author__ = "jboursier"
 __copyright__ = "Copyright 2025, Malwarebytes"
-__version__ = "1.7.0"
+__version__ = "1.8.0"
 __maintainer__ = "jboursier"
 __email__ = "jboursier@malwarebytes.com"
 __status__ = "Production"
